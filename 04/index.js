9f71113701fc4fa7d53858733658f1c7deb2f2f3
Unknown <gabrielqueirozzn@gmail.com>
2018-10-27 03:02:39 -0300

    Questão 04

---
+++ 04/index.js
@@ -0,0 +1,14 @@
+const createCheckDigit = nembershipId => {
+  let array = nembershipId.split("");
+  let result = array.reduce((prevVal, e) => {
+    return prevVal + parseFloat(e);
+  }, 0);
+  final = result.toString().split("");
+  if (final.length < 1) {
+    return;
+  }
+  return final.reduce((prev, e) => {
+    return prev + parseFloat(e);
+  }, 0);
+};
+console.log(createCheckDigit("55555"));
