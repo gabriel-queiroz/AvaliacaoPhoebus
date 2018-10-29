19214f97dd5806f44897c9575dbe86cb1eb7722b
Unknown <gabrielqueirozzn@gmail.com>
2018-10-28 23:01:06 -0300

    Questão 09

---
+++ 09/index.js
@@ -0,0 +1,10 @@
+const firstDuplicate = array => {
+  const repetidos = [];
+  const index = [];
+  const set = new Set(array);
+  if (set.size === array.length) {
+    return -1;
+  } else {
+    return array;
+  }
+};
